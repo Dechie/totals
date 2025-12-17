370a7709c692ac0e099b5a1aac962501007f1c9d
Dechasa <Dechu373@protonmail.com>
2025-12-17 17:20:58 +0300

    fix errors in insights provider.

--- app/lib/providers/insights_provider.dart
+++ app/lib/providers/insights_provider.dart
@@ -1,31 +1,42 @@
-
 import 'package:flutter/foundation.dart';
 
 import '../services/financial_insights.dart';
 import 'transaction_provider.dart';
 
 class InsightsProvider extends ChangeNotifier {
-  final TransactionProvider txProvider;
+  TransactionProvider _txProvider;
   late final InsightsService _service;
 
-  InsightsProvider({required this.txProvider}) {
+  InsightsProvider({required TransactionProvider txProvider})
+      : _txProvider = txProvider {
     // we will use live transactions from existing provider,
 
-    _service = InsightsService(() => txProvider.transactions);
-    txProvider.addListener(_onTxChange);
+    _service = InsightsService(() => _txProvider.transactions);
+    _txProvider.addListener(_onTxChange);
+  }
+
+  Map<String, dynamic> get insights => _service.summarize();
+
+  // allow ProxyProvider to update when TransactionProvider is updated.
+  set txProvider(TransactionProvider newProvider) {
+    if (identical(_txProvider, newProvider)) return;
+
+    _txProvider.removeListener(_onTxChange);
+    _txProvider = newProvider;
+    _txProvider.addListener(_onTxChange);
+    _service.invalidate(); // clear the cache
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _txProvider.removeListener(_onTxChange);
+    super.dispose();
   }
 
   void _onTxChange() {
     // Invalidate cache when tx data updates, then nofify listeners.
     _service.invalidate();
     notifyListeners();
-  }
-
-  Map<String, dynamic> get insights => _service.summarize();
-
-  @override
-  void dispose() {
-    txProvider.removeListener(_onTxChange);
-    super.dispose();
   }
 }
